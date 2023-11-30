d4513daf2497bf6fa9d7b51b1d210b8c5b262678
Eric Stine <ericstine@Erics-MacBook-Pro.local>
2023-11-29 20:09:56 -0500

    instead of the static tape appending, we go for a dynamically allocated tape process instead.

--- src/tm_simulator.py
+++ src/tm_simulator.py
@@ -4,20 +4,17 @@ def simulate_tm(tape_input, num_states, halt_state, transitions):
     This function prints the TM configurations until it reaches the halting state.
 
     Parameters:
-    - tape_input (list): The initial tape configuration including leading and trailing blanks.
+    - tape_input (list): The initial tape configuration including one leading and one trailing blank.
     - num_states (int): The total number of states in the TM.
     - halt_state (int): The halting state of the TM.
     - transitions (dict): The state transition rules.
     """
 
-    # Calculate the number of extra spaces to add based on the length of the input
-    inf_tape = len(tape_input)
+    # Initialize the tape with the input, which already includes a leading and trailing blank
+    tape = list(tape_input)
 
-    # Add extra space before and after the input
-    tape = ["_"] * inf_tape + tape_input + ["_"] * inf_tape
-
-    # Initialize the head position at the first blank before the actual binary input
-    head = inf_tape
+    # Initialize the head position on the first position of the input
+    head = 0
 
     # Check for invalid input parameters.
     if tape is None or num_states is None or halt_state is None or transitions is None:
@@ -36,20 +33,23 @@ def simulate_tm(tape_input, num_states, halt_state, transitions):
         print(f"{current_state}: {display_tape}")
 
         # Retrieve the transition rule for the current state and tape symbol.
-        write, move, next_state = transitions.get(
-            (current_state, tape[head]), (None, None, None)
-        )
+        current_symbol = tape[head]
+        transition_key = (current_state, current_symbol)
 
-        # Check for invalid transition.
-        if write is None:
-            print("Invalid transition. Entering error state and stopping.")
+        if transition_key not in transitions:
+            print(
+                f"No transition defined for state {current_state} and symbol '{current_symbol}'."
+            )
+            print("Entering error state and stopping.")
             return
+
+        write, move, next_state = transitions[transition_key]
 
         # Write to the tape and move the head.
         tape[head] = write
         head += 1 if move == "R" else -1
 
-        # Extend the tape if the head moves beyond the current bounds
+        # Dynamically extend the tape if the head moves beyond the current bounds
         if head < 0:
             tape.insert(0, "_")
             head = 0
